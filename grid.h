b4859a5dac11ec2d4f904d54f9e4b62313f92788
Tim Gatto <timjgatto@gmail.com>
2022-11-17 09:32:58 -0500

    cleanup in grid class

--- grid.h
+++ grid.h
@@ -9,16 +9,13 @@
 class Grid {
     private:
         Tile all_tiles[ constants::T_TILES ]; // contains all tiles in the grid
-            
         Tile copy_of_tiles[constants::T_TILES]; // clone of all tiles, used for making new generations/iterating
 
         // takes an (x, y) position an turns it into an id for the all_tiles array
         int id_from_pos( int x, int y ); 
-
         // checks if a given position is in the bounds of the array
         bool is_in_bounds( int x, int y);
-
-        
+        // returns the number of neighbors with value 1
         int alive_neighbors( int x, int y);
 
     public:
@@ -26,10 +23,9 @@ class Grid {
 
         // sets all the tiles to correct positions and changes their color
         void make_tiles();
-
         // calls the draw function for every tile in the all_tiles array
         void draw_all( SDL_Renderer *window_renderer );
-
+        // iterats to a new generation using the ruleset of Conway's Game of Life
         void iterate_conway();
 };
 
