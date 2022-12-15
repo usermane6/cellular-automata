9a2d74b391edf5eacf8b89ae48eb76dc32dafbcf
Tim Gatto <timjgatto@gmail.com>
2022-12-15 11:24:26 -0500

    minor tweaks

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 #include "constants.hpp"
 
 void display_fps(double fps, SDL_Renderer* window_renderer, TTF_Font* font, SDL_Surface* surface, SDL_Texture* texture) {
-    SDL_Color t_color = { 0, 0, 255, 0 };
+    // todo cleanup
+    SDL_Color t_color = { 0, 0, 0, 0 };
+    SDL_Color box_color = { 255, 255, 255, 0};
     char* fps_str = const_cast<char*>(std::to_string(fps).c_str()); // converts fps param to char*, that SDL_ttf uses
     
     surface = TTF_RenderText_Solid(font, fps_str, t_color);
@@ -15,7 +17,9 @@ void display_fps(double fps, SDL_Renderer* window_renderer, TTF_Font* font, SDL_
     int h, w; // height/width of text
     SDL_QueryTexture(texture, NULL, NULL, &w, &h);
     SDL_Rect rect = {0, 0, w, h};
-
+    SDL_SetRenderDrawColor(window_renderer, 255, 255, 255, 0);
+    SDL_RenderDrawRect(window_renderer, &rect);
+    SDL_RenderFillRect(window_renderer, &rect);
     SDL_RenderCopy(window_renderer, texture, NULL, &rect);
     SDL_RenderPresent(window_renderer);
 }
@@ -23,23 +27,24 @@ void display_fps(double fps, SDL_Renderer* window_renderer, TTF_Font* font, SDL_
 int main( int argc, char *argv[] ) {
     SDL_Init( SDL_INIT_EVERYTHING );
 
-    SDL_Window* window = SDL_CreateWindow( "Cells", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, constants::W_WIDTH, constants::W_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window* window = SDL_CreateWindow("Cells", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, constants::W_WIDTH, constants::W_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
     SDL_Renderer* m_window_renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     if ( window = NULL ) {
         std::cout << "Error Creating Window" << SDL_GetError() << "\n";
-        return 1;
+        return EXIT_FAILURE;
     }
 
     if (TTF_Init() == -1) {
-        std::cout << "Error Loading font" << TTF_GetError();
+        std::cout << "Error Loading font" << TTF_GetError() << "\n";
+        return EXIT_FAILURE;
     }
 
     TTF_Font* arial = TTF_OpenFont("DMMono-Medium.ttf", 24);
     SDL_Surface* text_surface;
     SDL_Texture* text_texture;
 
-    Grid m_grid( Grid::conway ); 
+    Grid m_grid( Grid::war ); 
     m_grid.draw_all(m_window_renderer);
 
     Uint32 start_time, frame_time;
